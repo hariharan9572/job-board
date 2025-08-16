750bbdfe4e05f967e4b2d9672883bee0360a3a85
Hariharan <hariharan9572@gmail.com>
2025-08-16 09:49:16 +0530

    job-board

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\JobApplicationController;
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\MyJobApplicationController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,3 +28,11 @@ Route::resource('auth', AuthController::class)
 Route::delete('logout', fn() => to_route('auth.destroy'))->name('logout');
 Route::delete('auth', [AuthController::class, 'destroy'])
     ->name('auth.destroy');
+
+Route::middleware('auth')->group(function(){
+    Route::resource('job.application', JobApplicationController::class)
+        ->only(['create', 'store']);
+
+    Route::resource('my-job-applications', MyJobApplicationController::class)
+        ->only(['index', 'destroy']);
+});
